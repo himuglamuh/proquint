d3f62a561632775a68cd2fb2a12abf1076172b9c
F3r41OutL4w <himuglamuh@proton.me>
2025-09-02 08:40:09 -0700

    fix(gitignore dist dir)

--- dist/hash.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getBytes = getBytes;
-const crypto_1 = require("crypto");
-function getBytes(input, algo) {
-    if (algo === "none")
-        return Buffer.from(input, "utf8");
-    return (0, crypto_1.createHash)(algo).update(input).digest();
-}
